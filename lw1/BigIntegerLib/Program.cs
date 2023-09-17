bb856e2dac2b963afaf816d39798bcb7b41e6f56
Egor <79940875+Bareen86@users.noreply.github.com>
2023-09-17 13:13:44 +0300

    Добавил класс BigInteger

---
+++ lw1/BigIntegerLib/Program.cs
@@ -0,0 +1,16 @@
+﻿using BigIntegerLib;
+
+BigInteger a = new BigInteger( "12345678901234567890" );
+BigInteger b = new BigInteger( "98765432109876543210" );
+
+Console.WriteLine( "a = " + a );
+Console.WriteLine( "b = " + b );
+
+Console.WriteLine( "a + b = " + ( a + b ) );
+Console.WriteLine( "a - b = " + ( a - b ) );
+Console.WriteLine( "a * b = " + ( a * b ) );
+Console.WriteLine( "a / b = " + ( a / b ) );
+Console.WriteLine( "a % b = " + ( a % b ) );
+Console.WriteLine( ( "a == b = " + ( a == b ) ) );
+Console.WriteLine( ( "a > b = " + ( a > b ) ) );
+Console.WriteLine( ( "a < b = " + ( a < b ) ) );
